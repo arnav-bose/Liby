4372eb69bc69c6fb7c7df7f7b7bd08a32a5fd636
Arnav <arnav-bose>
2016-05-03 19:55:09 +0530

    Profile Test

--- app/src/main/java/com/example/arnavbose/liby/FragmentProfile.java
+++ app/src/main/java/com/example/arnavbose/liby/FragmentProfile.java
@@ -32,7 +32,7 @@ public class FragmentProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.profile, container, false);
+        View view = inflater.inflate(R.layout.profile_test, container, false);
 
         setRetainInstance(true);
         tabLayoutProfile = (TabLayout)view.findViewById(R.id.tabLayoutProfile);
--- app/src/main/java/com/example/arnavbose/liby/FragmentStatus.java
+++ app/src/main/java/com/example/arnavbose/liby/FragmentStatus.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,10 @@ import java.util.ArrayList;
  */
 public class FragmentStatus extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -24,10 +30,27 @@ public class FragmentStatus extends Fragment {
         SharedPreferences sharedPreferences = getContext().getSharedPreferences("MyData", Context.MODE_PRIVATE);
         String borrowerNumber = sharedPreferences.getString("borrowerNumber", "");
 
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.recyclerViewCurrentIssues);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new RecyclerViewCurrentIssuesAdapter(getDataSetCurrentIssues());
+        mRecyclerView.setAdapter(mAdapter);
+
         String method = "Current Issues";
-        CurrentIssuesTask currentIssuesTask = new CurrentIssuesTask(getContext());
-        currentIssuesTask.execute(method, borrowerNumber);
+        //CurrentIssuesTask currentIssuesTask = new CurrentIssuesTask(getContext());
+        //currentIssuesTask.execute(method, borrowerNumber);
 
         return view;
+    }
+
+    private ArrayList<DataSetCurrentIssues> getDataSetCurrentIssues() {
+        ArrayList results = new ArrayList<DataSetCurrentIssues>();
+        for (int index = 0; index < 20; index++) {
+            DataSetCurrentIssues object = new DataSetCurrentIssues("<Title " + index + ">",
+                    "<Author " + index + ">", "Due Date: 20th May");
+            results.add(index, object);
+        }
+        return results;
     }
 }
